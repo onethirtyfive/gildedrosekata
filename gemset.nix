8a72fb39de4617fd0c6333f858a2bf38f6d01098
Joshua Morris <joshua.a.morris@gmail.com>
2017-02-09 12:38:04 -0800

    Gemfile, lockfile, and bundix gemset

---
+++ gemset.nix
@@ -0,0 +1,74 @@
+{
+  diff-lcs = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "18w22bjz424gzafv6nzv98h0aqkwz3d9xhm7cbr1wfbyas8zayza";
+      type = "gem";
+    };
+    version = "1.3";
+  };
+  given_core = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1lzyqllbbv6as3qgwz2007mvy7wy247bgkch9adnmh1zfa73bkrg";
+      type = "gem";
+    };
+    version = "3.8.0";
+  };
+  rspec = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "16g3mmih999f0b6vcz2c3qsc7ks5zy4lj1rzjh8hf6wk531nvc6s";
+      type = "gem";
+    };
+    version = "3.5.0";
+  };
+  rspec-core = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1nacs062qbr98fx6czf1vwppn1js956nv2c8vfwj6i65axdfs46i";
+      type = "gem";
+    };
+    version = "3.5.4";
+  };
+  rspec-expectations = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0bbqfrb1x8gmwf8x2xhhwvvlhwbbafq4isbvlibxi6jk602f09gs";
+      type = "gem";
+    };
+    version = "3.5.0";
+  };
+  rspec-given = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1783bazja10kbha8hk15khvybsq88siyax02cpkk688604h54nji";
+      type = "gem";
+    };
+    version = "3.8.0";
+  };
+  rspec-mocks = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0nl3ksivh9wwrjjd47z5dggrwx40v6gpb3a0gzbp1gs06a5dmk24";
+      type = "gem";
+    };
+    version = "3.5.0";
+  };
+  rspec-support = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "10vf3k3d472y573mag2kzfsfrf6rv355s13kadnpryk8d36yq5r0";
+      type = "gem";
+    };
+    version = "3.5.0";
+  };
+  sorcerer = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "09j22ykr9hyfvls374hc69byp9xpg5aarf52p9v0wwawm4jqphm8";
+      type = "gem";
+    };
+    version = "1.0.2";
+  };
+}
